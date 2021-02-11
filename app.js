41b98027437d3bd5e08770cd9465b50e40f09e82
Jennifer <jennifacarey@gmail.com>
2021-02-11 16:24:03 +0000

    prompt

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ const dogImg = document.querySelector("#dog-img");
 const catImg = document.querySelector("#cat-img");
 const rabbitImg = document.querySelector("#rabbit-img");
 
+// prompt box triggered when user selects image of any animal
+// const petName = prompt("Aw, great choice! Now enter a name for your pet.", "Your pet ");
+// }
+
+
+dogImg.addEventListener("click", () => {
+   let petName = prompt("Aw, great choice! Now enter a name for your pet.", "Your pet ");
+   let newDog = new Dog(`${petName}`);
+   console.log(newDog.name);
+})
