87975eb9cf234f30ad8ba9d208372859f17e2b11
Jennifer <jennifacarey@gmail.com>
2021-02-11 15:18:43 +0000

    add variables for animal images

---
+++ app.js
@@ -0,0 +1,5 @@
+// elements the user will interact with
+const dogImg = document.querySelector("#dog-img");
+const catImg = document.querySelector("#cat-img");
+const rabbitImg = document.querySelector("#rabbit-img");
+
