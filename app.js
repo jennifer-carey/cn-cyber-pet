5e2714af852a83f665531f5be742171b12eb054b
Jennifer <jennifacarey@gmail.com>
2021-02-12 16:35:51 +0000

    updates

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ const welcomeScreen = document.querySelector("#welcome-screen");
 const petInterface = document.querySelector("#pet-interface");
 const insertName = document.querySelector("#insert-name");
 const happinessLevel = document.querySelector("#happiness-level");
+const petEmotion = document.querySelector("#pet-emotion");
 // action buttons
 const feedBtn = document.querySelector("#feed-button");
 const playBtn = document.querySelector("#play-button");
 const washBtn = document.querySelector("#wash-button");
 const strokeBtn = document.querySelector("#stroke-button");
+
+let pet;
 
 petInterface.style.display = "none";
 
@@ -19,7 +22,60 @@ dogImg.addEventListener("click", () => {
    welcomeScreen.style.display = "none";
    petInterface.style.display = "block";
    let petName = prompt("Aw, great choice! Now enter a name for your pet.", "Your pet ");
-   let newDog = new Dog(`${petName}`, 10);
-   insertName.textContent = newDog.name;
-   happinessLevel.textContent = newDog.happiness;
+   pet = new Dog(petName);
+   insertName.textContent = pet.name;
+   happinessLevel.textContent = pet.happiness;
+   // setInterval(() => {
+   //    console.log("game loop");
+   // }, 3000);
 });
+
+catImg.addEventListener("click", () => {
+   welcomeScreen.style.display = "none";
+   petInterface.style.display = "block";
+   let petName = prompt("Aw, great choice! Now enter a name for your pet.", "Your pet ");
+   pet = new Cat(petName);
+   insertName.textContent = pet.name;
+   happinessLevel.textContent = pet.happiness;
+   // setInterval(() => {
+   //    console.log("game loop");
+   // }, 3000);
+});
+
+rabbitImg.addEventListener("click", () => {
+   welcomeScreen.style.display = "none";
+   petInterface.style.display = "block";
+   let petName = prompt("Aw, great choice! Now enter a name for your pet.", "Your pet ");
+   pet = new Rabbit(petName);
+   insertName.textContent = pet.name;
+   happinessLevel.textContent = pet.happiness;
+   // setInterval(() => {
+   //    console.log("game loop");
+   // }, 3000);
+});
+
+feedBtn.addEventListener("click", () => {
+   petEmotion.style.display = "block";
+   petEmotion.textContent = `${pet.name} loves food!`;
+   pet.feed();
+})
+
+playBtn.addEventListener("click", () => {
+   petEmotion.style.display = "block";
+   petEmotion.textContent = `${pet.name} loves playing!`;
+   pet.play();
+})
+
+washBtn.addEventListener("click", () => {
+   petEmotion.style.display = "block";
+   petEmotion.textContent = `Uh-oh! ${pet.name} hates bath time.`;
+   pet.wash();
+})
+
+strokeBtn.addEventListener("click", () => {
+   petEmotion.style.display = "block";
+   petEmotion.textContent = `${pet.name} loves cuddles!`;
+   pet.stroke();
+})
+
+/// create start again option
