626c37fcfbe6173a2a79036c7848cbd86b034eae
Jennifer <jennifacarey@gmail.com>
2021-02-12 13:16:35 +0000

    set getter to capitalise first letter of name

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 // create animal template
 
 class Animal {
-   constructor(name, happiness) {
-      this.name = name;
-      this.happiness = happiness;
+   constructor(name) {
+      this._name = name;
    }
+   get name() {
+      return this._name.charAt(0).toUpperCase() + this._name.slice(1);
+   }
+
    feed() {
       //print to DOM
       console.log(`${this.name} loves feeding time!`);
